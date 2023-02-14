65b028159f1cd1535210cf751358592fbfaaa467
Persistent-A <apurva.mili@gmail.com>
2023-02-13 22:35:33 -0500

    Github LinkedIn and  Mail href inclusion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
         <div>Apurva &copy; 2023</div>
         <div>
             <div>
-                <a href="adsffadgaf" target='_blank' rel="noopener norefefrer"><FiGithub /></a>
-                <a href="adsffadgaf" target='_blank' rel="noopener norefefrer"><FiLinkedin /></a>
-                <a href="adsffadgaf" target='_blank' rel="noopener norefefrer"><FiMail /></a>
+                <a href="https://github.com/Persistent-A" target='_blank' rel="noopener noreferrer"><FiGithub /></a>
+                <a href="https://www.linkedin.com/in/apurva-apurva-70509674?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BoKw4Mbx0RkOujlE71ZlRVA%3D%3D" target='_blank' rel="noopener noreferrer"><FiLinkedin /></a>
+                <a href="mailto:apurva.mili@gmail.com" target='_blank' rel="noopener noreferrer"><FiMail /></a>
             </div>
             <div>Developed by Apurva</div>
         </div>
