6b9f099eccdf959f48d7767bc2e2065d134d77bd
Persistent-A <apurva.mili@gmail.com>
2023-02-14 00:20:48 -0500

    Current work focus description typo correction

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
         <div>Hi, I'm Apurva</div>
         <div>Full Stack Developer</div>
         <div>I am a student of Web-Development at TAV College, Montreal.
-            Currently I'am focused towards learning technologies related to web such as React JS and Angular JS.
+            Currently I'am focused towards learning technologies related to web development such as React JS and Angular JS.
         </div>
         <div>
           <a href="mailto:apurva.mili@gmail.com">Contact Me <CiMail className="mail-icon"/></a>
