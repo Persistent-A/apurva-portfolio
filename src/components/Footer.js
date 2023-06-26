263fc854ef53a1345c4bbbe2cc15e1f5bb8abcce
Persistent-A <apurva.mili@gmail.com>
2023-06-26 15:24:25 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
                 <a href="https://www.linkedin.com/in/apurva-apurva-70509674?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BoKw4Mbx0RkOujlE71ZlRVA%3D%3D" target='_blank' rel="noopener noreferrer"><FiLinkedin /></a>
                 <a href="mailto:apurva.mili@gmail.com" target='_blank' rel="noopener noreferrer"><FiMail /></a>
             </div>
-            <div>Developed by Apurva</div>
+            <div className="footer-dev">Developed by Apurva</div>
         </div>
     </div>
   )
